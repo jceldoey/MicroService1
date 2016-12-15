88bb3cc78741c4d4ea7537263883aaf33608821e
Jens Christian Eldøy <Jens.christian.Eldoy@knowit.no>
2016-12-15 12:34:12 +0100

    Added tests

--- src/main/java/no/jceldoey/app/controller/UserController.java
+++ src/main/java/no/jceldoey/app/controller/UserController.java
@@ -1,9 +1,29 @@
 package no.jceldoey.app.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/user")
@@ -16,4 +36,50 @@ public class UserController {
 
 		return response;
 	}
+
+	@RequestMapping(value = "/{name}/image", method = RequestMethod.GET)
+	@ResponseBody
+	public ResponseEntity<String> getImage(@PathVariable String name){
+
+		String response = "Hi there, " + name + ". You want an image?";
+		
+		try {
+			URL url = new URL("https://api.github.com/users/" + name);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			
+			InputStream is = conn.getInputStream();
+			BufferedReader buffer = new BufferedReader(new InputStreamReader(is));
+			String jsonData = buffer.lines().collect(Collectors.joining("\n"));
+			is.close();
+			conn.disconnect();
+			
+			JsonNode rootNode = new ObjectMapper().readTree(new StringReader(jsonData));
+			JsonNode avatarNode = rootNode.path("avatar_url");
+			String avatarUrl = avatarNode.asText();
+			System.out.println("avatar = " + avatarUrl);
+			
+			response += " " + avatarNode.asText();
+			
+//			url = new URL(avatarUrl);
+//			conn = (HttpURLConnection) url.openConnection();
+			
+//			ByteBuffer bb = new ByteB
+//			byte[] image = conn.getInputStream().
+			
+//			org.springframework.http.HttpHeaders headers = new HttpHeaders();
+//		    headers.setContentType(MediaType.IMAGE_JPEG);
+//		    headers.setContentLength(image.length);
+		    
+		    return new ResponseEntity<>(response, null);
+					
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			response = e.getMessage();
+		}
+
+		return null;
+	}
+
 }
--- src/test/java/no/jceldoey/app/MicroService1ApplicationTests.java
+++ src/test/java/no/jceldoey/app/MicroService1ApplicationTests.java
@@ -3,7 +3,11 @@ package no.jceldoey.app;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Assert;
+
+import no.jceldoey.app.controller.UserController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +15,14 @@ public class MicroService1ApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void getAvatarUrl() throws Exception {
+		
+		ResponseEntity<String> response = new UserController().getImage("jceldoey");
+		
+		Assert.isNull(response, "The image avatar object must not be null");
 	}
 
 }
